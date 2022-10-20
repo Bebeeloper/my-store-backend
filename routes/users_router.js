6d3f2617b7d01b0c2b18e75398359f0d3e6dc8ab
bebeloper <kevind171819@gmail.com>
2022-10-20 00:39:13 -0500

    users login

--- routes/users_router.js
+++ routes/users_router.js
@@ -1,28 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-const user = {
+const users = [{
   userName: 'kmospina',
   password: '123'
-};
+},{
+  userName: 'mmendez',
+  password: '321'
+}];
 
 // Show list of users
 router.get('/', (req, res) => {
-  res.send('Users list')
+  res.send(users)
 });
 
 // Validate user login 'simulation'
 router.get('/:userName/:password', (req, res) => {
+  const usersToShow = [];
+  let logged = false;
+
   const { userName, password } = req.params;
-  // res.statusMessage = 'aleluya';
-  if (userName == 'kmospina' && password == '123') {
-    res.json(user);
-    res.status(200);
-    res.statusMessage = 'Aleluya';
-    console.log(res.statusMessage);
-  }else{
-    res.send('UserName or password wrong...');
+  // res.send(users[0].userName);
+
+  for (let index = 0; index < users.length; index++) {
+    if (users[index].userName == userName && users[index].password == password) {
+      usersToShow.push(users[index]);
+      logged = true;
+    }
   }
+
+  console.log(logged);
+  if (logged) {
+    return res.json(usersToShow);
+  }else{
+    return res.send('Passwor or userName wrong');
+  }
+
 });
 
 module.exports = router;
