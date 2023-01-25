938d8e49e7c8127394406ee08492e24f25ea8d8b
bebeloper <kevind171819@gmail.com>
2023-01-25 16:38:32 -0500

    Programming account module

--- routes/accounts_router.js
+++ routes/accounts_router.js
@@ -24,4 +24,18 @@ router.get('/:document', async (req, res) => {
   }
 });
 
+// POST Account
+router.post('/',  async (req, res) => {
+  try {
+    const body = req.body;
+    const accounts = await accService.postAccount(body);
+    res.status(200).json(accounts);
+  } catch (error) {
+    res.status(404).json({
+      ErrorMessage: error.message
+    });
+  }
+
+});
+
 module.exports = router;
--- services/accounts_services.js
+++ services/accounts_services.js
@@ -27,6 +27,31 @@ class AccountsServices {
     }
   }
 
+  // Post account
+  async postAccount(body){
+    if (Object.keys(body).length != 0) {
+      const queryInsertAcc = 'INSERT INTO accounts (first_name, last_name, document_number, email) VALUES ($1, $2, $3, $4) RETURNING *';
+      const arrayInsertAcc = [body.first_name, body.last_name, body.document_number, body.email];
+      const responseDB = await this.pool.query(queryInsertAcc, arrayInsertAcc);
+
+      if (responseDB.rows.length > 0) {
+        return {
+          message: 'Account created successfully',
+          data: responseDB.rows
+        }
+      }
+    }else{
+      throw new Error('Debes poner un body en formato JSON');
+    }
+  }
+
+  async getDBById(productId){
+    const query = 'SELECT * FROM accounts WHERE id = $1';
+    const array = [parseInt(productId)];
+    const responseDB = await this.pool.query(query, array);
+    return responseDB.rows;
+  }
+
 }
 
 module.exports = AccountsServices;
